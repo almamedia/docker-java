2a5f2e9c8adc7ac0be9653d03bfce9faafd64ad9
Tommi Liittokivi <tommi.liittokivi@conmio.com>
2017-08-09 14:42:53 +0300

    Initial docker files

---
+++ 7-jdk/script/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+USER_ID=${LOCAL_USER_ID:-9001}
+
+useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
+export HOME=/home/user
+chown user /app
+chown user /home/user
+
+if [ -z $1 ]; then
+	exec /usr/local/bin/gosu user bash
+else
+	exec /usr/local/bin/gosu user "$@"
+fi
+
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# docker-java
+
+#### Running /path_to_jar/runnable.jar:
+
+```
+docker run --rm \
+ -e LOCAL_USER_ID=`id -u $USER`\
+ -v /path_to_jar:/app \
+  conmio/java java -jar runnable.jar
+ ```
